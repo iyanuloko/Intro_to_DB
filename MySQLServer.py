2bef1457ebc5951fef223d5f4cf8c223b8df8981
iyanuloko <iyanuoluwaloko@gmail.com>
2025-08-23 19:52:30 +0100

    Second exercise edit 1

---
+++ MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+# Replace with your connection details
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="deltaforce1.",
+)
+
+mycursor = mydb.cursor()
+try:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except:
+    print("Database was not created.")
+
+mycursor.close()
+mydb.close()
